9345244d934aa8d8d441ff4124bd16873665edb4
Simona <localhost>
2011-05-24 20:48:07 +0300

    update bd

--- baza_librarie.sql
+++ baza_librarie.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 12, 2011 at 09:44 
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+-- Generation Time: May 24, 2011 at 08:46 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -54,43 +54,56 @@ INSERT INTO `autor` (`id`, `nume`, `prenume`, `origine`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `autorcarte`
+--
+
+CREATE TABLE IF NOT EXISTS `autorcarte` (
+  `idautor` int(5) NOT NULL,
+  `idcarte` int(5) NOT NULL,
+  PRIMARY KEY (`idautor`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `autorcarte`
+--
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `carte`
 --
 
 CREATE TABLE IF NOT EXISTS `carte` (
-  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `isbn` int(4) NOT NULL,
   `titlu` varchar(100) NOT NULL,
-  `idautor` int(4) NOT NULL,
+  `colectie` varchar(30) NOT NULL,
   `ideditura` int(4) NOT NULL,
-  `iddomeniu` int(4) NOT NULL,
   `nrpag` int(4) NOT NULL,
   `pret` int(4) NOT NULL,
   `nrbuc` int(4) NOT NULL,
-  `idlimba` int(4) NOT NULL,
+  `limba` varchar(30) NOT NULL,
   `anaparitie` int(4) NOT NULL,
   `descriere` varchar(255) NOT NULL,
   `imagine` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=139 ;
+  PRIMARY KEY (`isbn`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `carte`
 --
 
-INSERT INTO `carte` (`id`, `titlu`, `idautor`, `ideditura`, `iddomeniu`, `nrpag`, `pret`, `nrbuc`, `idlimba`, `anaparitie`, `descriere`, `imagine`) VALUES
-(1, 'Casa umbrelor', 5, 3, 3, 640, 80, 15, 1, 2009, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has su', 'imagini/banner6.jpg'),
-(2, 'PC pas cu pas', 2, 1, 1, 180, 24, 15, 1, 2008, '', ''),
-(3, 'Bazele programarii in C', 6, 1, 1, 367, 39, 5, 1, 2006, '', ''),
-(121, 'Informatica. Manual pentru clasa a X-a', 1, 1, 5, 180, 10, 10, 1, 2000, '', ''),
-(135, 'asdqwe', 67, 3, 11, 81, 82, 12, 1, 83, '', ''),
-(130, 'a1a', 68, 62, 11, 45, 4, 4, 74, 2, '', ''),
-(122, 'Programarea in limbajul C/C ++ pentru liceu', 1, 1, 1, 256, 30, 23, 1, 2010, '', ''),
-(136, 'aaaa', 67, 3, 7, 23, 12, 5, 12, 2001, '', ''),
-(123, 'PROGRAMAREA IN C#', 64, 3, 1, 324, 17, 3, 2, 2003, '', ''),
-(124, 'CHARLIE WILCOX', 65, 3, 3, 189, 14, 35, 2, 2003, '', ''),
-(125, '100 DE PERSONALITATI ALE SECOLULUI XX. PICTORI', 66, 3, 6, 270, 25, 15, 74, 2001, '', ''),
-(137, 'asd', 71, 63, 4, 34, 45, 76676, 75, 656, '', ''),
-(138, 'carte test', 2, 2, 2, 2, 2, 2, 2, 2, '', '');
+INSERT INTO `carte` (`isbn`, `titlu`, `colectie`, `ideditura`, `nrpag`, `pret`, `nrbuc`, `limba`, `anaparitie`, `descriere`, `imagine`) VALUES
+(1, 'Casa umbrelor', '', 3, 640, 80, 15, 'romana', 2009, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has su', 'imagini/banner6.jpg'),
+(2, 'PC pas cu pas', '', 1, 180, 24, 15, 'romana', 2008, '', ''),
+(3, 'Bazele programarii in C', '', 1, 367, 39, 5, '1', 2006, '', ''),
+(121, 'Informatica. Manual pentru clasa a X-a', '', 1, 180, 10, 10, '1', 2000, '', ''),
+(122, 'Programarea in limbajul C/C ++ pentru liceu', '', 1, 256, 30, 23, '1', 2010, '', ''),
+(123, 'PROGRAMAREA IN C#', '', 3, 324, 17, 3, '2', 2003, '', ''),
+(124, 'CHARLIE WILCOX', '', 3, 189, 14, 35, '2', 2003, '', ''),
+(125, '100 DE PERSONALITATI ALE SECOLULUI XX. PICTORI', '', 3, 270, 25, 15, '74', 2001, '', ''),
+(0, '', '', 0, 0, 0, 0, '', 0, '', ''),
+(44444444, 'egegedg', '', 57, 0, 0, 0, '', 0, '', '');
 
 -- --------------------------------------------------------
 
@@ -102,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `domeniu` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
   `denumire` varchar(40) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
 
 --
 -- Dumping data for table `domeniu`
@@ -113,10 +126,19 @@ INSERT INTO `domeniu` (`id`, `denumire`) VALUES
 (2, 'Hobby'),
 (3, 'Beletristica'),
 (4, 'Psihologie'),
-(5, 'Carte scolara'),
 (6, 'Arte'),
 (7, 'Gastronomie'),
-(8, 'Science Fiction');
+(19, 'Medicina - Sanatate'),
+(15, 'Autoeducare'),
+(16, 'Economie'),
+(17, 'Stiinte juridice'),
+(18, 'Istorie'),
+(20, 'Religie'),
+(21, 'Educatie - Invatamant'),
+(22, 'Stiinta - Tehnica'),
+(23, 'Carte straina'),
+(24, 'Politica'),
+(25, 'Limbi straine');
 
 -- --------------------------------------------------------
 
@@ -131,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `editura` (
   `nrtelefon` varchar(10) NOT NULL,
   `email` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=64 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=68 ;
 
 --
 -- Dumping data for table `editura`
@@ -146,34 +168,10 @@ INSERT INTO `editura` (`id`, `denumire`, `localitate`, `nrtelefon`, `email`) VAL
 (56, 'Libris', 'Brasov', '0268477701', 'vanzari@libris.ro'),
 (57, 'Teora', 'Bucuresti', '0318100007', ''),
 (58, 'Nemira', 'Bucuresti', '0212242156', 'librarie@nemira.ro'),
-(63, 'zzzz', 'eeee', '232443', 'dfdvgdf');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `limba`
---
-
-CREATE TABLE IF NOT EXISTS `limba` (
-  `id` int(4) NOT NULL AUTO_INCREMENT,
-  `denumire` varchar(15) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=76 ;
-
---
--- Dumping data for table `limba`
---
-
-INSERT INTO `limba` (`id`, `denumire`) VALUES
-(1, 'romana'),
-(2, 'engleza'),
-(3, 'franceza'),
-(4, 'germana'),
-(12, 'chineza'),
-(21, 'rusa'),
-(67, 'japoneza'),
-(75, 'dddddd'),
-(74, 'maghiara');
+(64, 'dddd', '', '', ''),
+(65, 'jjjj', 'efd', '4356', 'dgrhr'),
+(66, 'jjjj', 'efd', '4356', 'dgrhr'),
+(67, 'aaaaa', 'dfgfd', '435', 'gdgd');
 
 -- --------------------------------------------------------
 
@@ -183,12 +181,81 @@ INSERT INTO `limba` (`id`, `denumire`) VALUES
 
 CREATE TABLE IF NOT EXISTS `subdomeniu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `denumire` int(11) NOT NULL,
+  `denumire` varchar(30) NOT NULL,
   `idDomeniu` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=48 ;
 
 --
 -- Dumping data for table `subdomeniu`
 --
 
+INSERT INTO `subdomeniu` (`id`, `denumire`, `idDomeniu`) VALUES
+(1, 'Arhitectura', 6),
+(2, 'Fotografie', 6),
+(3, 'Muzica', 6),
+(4, 'Pictura', 6),
+(5, 'Sculptura', 6),
+(6, 'Grafica', 6),
+(7, 'Istoria artei', 6),
+(8, 'Albume de arta', 6),
+(9, 'Dans', 6),
+(10, 'Teatru', 6),
+(11, 'Cinematografie', 6),
+(12, 'Criminalistica', 17),
+(13, 'Drept administrativ', 17),
+(14, 'Drept civil', 17),
+(15, 'Legislatie', 17),
+(16, 'Penal', 17),
+(17, 'Drept comercial', 17),
+(18, 'Drept bancar', 17),
+(19, 'Crestinism', 20),
+(20, 'Islam', 20),
+(21, 'Iudaism', 20),
+(22, 'Istoria religiilor', 20),
+(23, 'Studii si eseuri teologice', 20),
+(24, 'Arte si tehnici culinare', 7),
+(25, 'Bauturi', 7),
+(26, 'Bucatari celebri', 7),
+(27, 'Diete', 7),
+(28, 'Alimentatie vegetariana', 7),
+(29, 'Imbunatatirea memoriei', 15),
+(30, 'Managementul stressului', 15),
+(31, 'Spiritual', 15),
+(32, 'Motivational', 15),
+(33, 'Transformare personala', 15),
+(34, 'Administratie publica', 16),
+(35, 'Finante - Banci', 16),
+(36, 'Economie politica', 16),
+(37, 'Management', 16),
+(38, 'Afaceri', 16),
+(39, 'Contabilitate', 16),
+(40, 'Legislatie / Fiscalitate', 16),
+(41, 'Marketing / Publicitate', 16),
+(42, 'Cardiologie', 19),
+(43, 'Oftalmologie', 19),
+(44, 'ORL', 19),
+(45, 'Stomatologie', 19),
+(46, 'Alimentatie', 19),
+(47, 'Dietoterapie', 19);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subdomeniucarte`
+--
+
+CREATE TABLE IF NOT EXISTS `subdomeniucarte` (
+  `idsubdomeniu` int(5) NOT NULL,
+  `idcarte` int(5) NOT NULL,
+  PRIMARY KEY (`idsubdomeniu`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subdomeniucarte`
+--
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
