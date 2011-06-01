660f106d443cce638939b5e44ce651e87747f0df
Simona <simonaleuta@yahoo.com>
2011-06-01 20:53:02 +0300

    adauga carte, index si login2

---
+++ proiect/login2.php
@@ -0,0 +1,147 @@
+
+ <td class="content_width_td">                           	
+                        
+               
+
+			
+
+
+<form name="login" action="http://osc3.template-help.com/osc_23506/login.php?action=process" method="post">	
+
+
+  				<table cellpadding="0" cellspacing="0" border="0" class="cont_heading_table">
+					<tr><td class="cont_heading_td">
+				Welcome, Please Sign In			
+</td></tr>
+				</table>								
+
+  		<table cellpadding="0" cellspacing="0" border="0" class="content_wrapper1_b">
+			<tr><td class="content_wrapper_r">
+				<div class="content_wrapper_t">
+					<div class="content_wrapper_l">
+						<div class="content_wrapper_tl">
+							<div class="content_wrapper_tr">
+								<div class="content_wrapper_bl">
+									<div class="content_wrapper_br">
+										<div class="width_100"> 
+<!--
+   <table cellpadding="0" cellspacing="0" border="0">
+	  <tr><td style="padding:0px 10px 0px 10px;" class="smallText">
+	  <font color="#ff0000"><b>Note:</b></font> Your &quot;Visitors Cart&quot; contents will be merged with your &quot;Members Cart&quot; contents once you have logged on. <a href="javascript:session_win();">[More Info]</a></td></tr>
+	</table>
+	
+-->
+
+<div style="padding:0px 0px 4px 0px;"><img src="images/spacer.gif" tppabs="http://osc3.template-help.com/osc_23506/images/spacer.gif" border="0" alt="" width="1" height="1"></div>	
+	<table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="main indent_2" width="50%"><b>Sunt client nou</b></td>
+			<td><img src="images/pixel_trans.gif" border="0" alt="" width="10" height="1"></td>
+            <td class="main indent_2" width="50%"><b>Sunt deja client</b></td>
+          </tr>
+          <tr>
+            <td width="50%" height="100%">
+				
+
+  		<table cellpadding="0" cellspacing="0" border="0" class="pic22_b infoBox_">
+			<tr><td class="pic_r">
+				<div class="pic_t">
+					<div class="pic_l">
+						<div class="pic_tl">
+							<div class="pic_tr">
+								<div class="pic_bl">
+									<div class="pic_br">
+										<div class="width_100">
+				<table border="0" width="100%" height="100%" cellspacing="5" cellpadding="0" style="height:240px;">
+                  <tr>
+                    <td><img src="images/pixel_trans.gif" border="0" alt="" width="100%" height="10"></td>
+                  </tr>
+                  <tr>
+                    <td class="main" style="height:100%;"><br><br>Prin crearea unui cont pe Librarie Online puteti face cumparaturi mai repede, sunteti la curent cu starea comenzilor dvs. </td>
+                  </tr>
+                  <tr>
+                    <td><img src="images/pixel_trans.gif" border="0" alt="" width="100%" height="10"></td>
+                  </tr>
+                  <tr>
+                    <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+                      <tr>
+                        <td width="10"><img src="images/pixel_trans.gif" border="0" alt="" width="10" height="1"></td>
+                        <td align="right"><a href="create_account.php">
+							<img src="includes/languages/english/images/buttons/button_continue.gif" border="0" alt="Continue" title=" Continue " width="79" height="24"></a></td>
+                        <td width="10"><img src="images/pixel_trans.gif" border="0" alt="" width="10" height="1"></td>
+                      </tr>
+                    </table>
+					
+					</td>
+                  </tr>
+                </table>
+				
+</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</td></tr>
+		</table>
+			</td>
+			<td><img src="images/pixel_trans.gif" border="0" alt="" width="1" height="1"></td>
+            <td width="50%" height="100%">
+				
+
+  		<table cellpadding="0" cellspacing="0" border="0" class="pic22_b infoBox_">
+			<tr><td class="pic_r">
+				<div class="pic_t">
+					<div class="pic_l">
+						<div class="pic_tl">
+							<div class="pic_tr">
+								<div class="pic_bl">
+									<div class="pic_br">
+										<div class="width_100">
+				<table border="0" width="100%" height="100%" cellspacing="5" cellpadding="0" style="height:240px;">
+                  <tr><td><img src="images/pixel_trans.gif" border="0" alt="" width="100%" height="10"></td></tr>
+                  <tr> <td class="main" colspan="2"></td></tr>
+                  <tr><td><img src="images/pixel_trans.gif" border="0" alt="" width="100%" height="10"></td></tr>
+                  <tr><td class="main"><b>Adresa de e-mail:</b></td></tr>
+                  <tr><td class="main"><input type="text" name="email_address"></td></tr>
+                  <tr><td class="main"><b>Parola:</b></td></tr>
+                    <td class="main"><input type="password" name="password" maxlength="40"></td></tr>
+                  <tr><td colspan="2"><img src="images/pixel_trans.gif" border="0" alt="" width="100%" height="10"></td></tr>
+                  <tr><td class="smallText"><a href="password_forgotten.php">Ati uitat parola?.</a></td></tr>
+                  <tr><td><img src="images/pixel_trans.gif" border="0" alt="" width="100%" height="10"></td></tr>
+                  <tr><td>
+                  	<table border="0" width="100%" cellspacing="0" cellpadding="2">
+                      <tr>
+                        <td width="10"><img src="images/pixel_trans.gif" border="0" alt="" width="10" height="1"></td>
+                        <td align="right"><input type="image" src="includes/languages/english/images/buttons/button_login1.gif" border="0" alt="Sign In" title=" Sign In "></td>
+                        <td width="10"><img src="images/pixel_trans.gif" border="0" alt="" width="10" height="1"></td>
+                      </tr>
+                    </table>
+                  </td></tr>
+                </table>
+				
+</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</td></tr>
+		</table>
+			</td>
+          </tr>
+        </table>
+	
+</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</td></tr>
+		</table>      		
+
+	</form></td>
