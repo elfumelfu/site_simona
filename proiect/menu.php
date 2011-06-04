fc2e09c3bc7c6fd1d2fa1ab76b842e624d79cc9e
Simona <simonaleuta@yahoo.com>
2011-06-04 12:17:41 +0300

    update index si menu proiect

--- proiect/menu.php
+++ proiect/menu.php
@@ -1,7 +1,7 @@
 <table border="0" width="100%" cellspacing="0" cellpadding="0"  class="infoBoxHeading_table">
   <tr>
     <td><img src="images/corner_top_left.gif" border="0" alt="" width="4" height="4"></td>
-    <td  class="infoBoxHeading_td">Categorii</td>
+    <td  class="infoBoxHeading_td">Domenii</td>
     <td><img src="images/corner_top_right.gif" border="0" alt="" width="4" height="4"></td>
   </tr>
 </table>
@@ -11,13 +11,15 @@
   <tr>
     <td  class="boxText">
 	<ul class="categories">
-	<?php	//Listarea categoriilor
+	<?php	//Listarea domeniilor
 	include("../admin/connect.php");
 	if (isset($_GET['domeniu']))
 		$sel_domeniu = $_GET['domeniu'];
 	else
 		$sel_domeniu = '';
-	$lista_categorii = "SELECT denumire from `baza_librarie`.`domeniu` ORDER BY denumire ";
+	$lista_categorii = "SELECT denumire from `baza_librarie`.`domeniu`
+						WHERE denumire<>'Colectie'
+						ORDER BY denumire ";
 		$test = mysql_query($lista_categorii);
 	
 		while($domeniu = mysql_fetch_array($test)) {
@@ -28,7 +30,8 @@
 				echo '<li class="bg_list"><a href="'.$_SERVER['PHP_SELF'].'?domeniu='.$domeniu['denumire'].'">'.$domeniu['denumire'].'</a></li>';
 				
 				$lista_subcategorii = "SELECT sub.denumire as denumire from `baza_librarie`.`domeniu` as dom,`baza_librarie`.`subdomeniu` as sub 
-				                      WHERE sub.idDomeniu=dom.id and dom.denumire='".$sel_domeniu."' ORDER BY denumire";
+				                      WHERE sub.idDomeniu=dom.id and dom.denumire='".$sel_domeniu."' and sub.denumire<>'-'  
+									  ORDER BY denumire";
 				$testsub = mysql_query($lista_subcategorii);
 				//var_dump($lista_subcategorii);
 				echo '<ul class="subcategories">';
